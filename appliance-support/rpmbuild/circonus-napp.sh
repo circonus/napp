242a8efca515c0f123e2d30d12a532d6caad4a13
Sergey Ivanov <seriv@omniti.com>
2010-03-22 16:00:32 +0000

    release bump and changelog for previous change

--- appliance-support/rpmbuild/circonus-napp.sh
+++ appliance-support/rpmbuild/circonus-napp.sh
@@ -31,7 +31,7 @@ cp `dirname $0`/napp-httpd ${TOPDIR}/SOURCES/
 cp `dirname $0`/issue-refresh-initscript.patch ${TOPDIR}/SOURCES/
 sed -e "s/@@REV@@/$REV/" -e "s/@@REV2@@/$REV2/" -e "s/@@DEPLOY@@/$DEPLOY/" <<EOF  > $SPECFILE
 %define		rversion	0.1r@@REV@@
-%define		rrelease	0.1
+%define		rrelease	0.2
 Name:		circonus-napp
 Version:	%{rversion}
 Release:	%{rrelease}
@@ -173,6 +173,8 @@ fi
 
 
 %changelog
+* Mon Mar 22 2010 Sergey Ivanov <seriv@omniti.com> - 0.1r3711-0.2
+- Set selinux context etc_t for everything under /opt/noit/etc/
 * Wed Mar 10 2010 Sergey Ivanov <seriv@omniti.com> - 0.1r3711-0.1
 - Initial package.
 EOF
