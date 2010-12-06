dd2f73a721dee5c156d074376fef2e490b81ba19
Theo Schlossnagle <jesus@omniti.com>
2010-12-06 06:44:55 +0000

    sol11 stuff for joyent agent

---
+++ appliance-root/opt/napp/bin/sol11-canupdate.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+CURL=curl
+for i in /opt/omni/bin /opt/local/bin
+do
+	if [ -x "$i/curl" ]; then
+		CURL="$i/curl"
+		break
+	fi
+done
+BASE=http://updates.circonus.com/joyent/5.11
+UPDATES_AVAILABLE=0
+
+handle_packages() {
+	while [ -n "$*" ];
+	do
+		pkg=$1
+		ver=$2
+		file=$3
+		shift 3
+		cver=`pkginfo -l $pkg 2>&1 | awk '{if($1 == "VERSION:") { print $2;}}'`
+		if [ "$ver" != "$cver" ]; then
+			UPDATES_AVAILABLE=1
+			if [ -n "$cver" ]; then
+				echo "removing $pkg"
+			fi
+			if [ -n "$ver" ]; then
+				echo "$pkg $ver circonus"
+			else
+				echo "$pkg (remove) circonus"
+			fi
+		fi
+	done
+}
+
+handle_packages `$CURL -s $BASE/ea.pkgs`
+
+if [ "$UPDATES_AVAILABLE" = "0" ]; then
+	echo "No updates."
+fi
---
+++ appliance-root/opt/napp/bin/sol11-update.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+CURL=curl
+BASE=http://updates.circonus.com/joyent/5.11
+UPDATES_AVAILABLE=0
+
+handle_packages() {
+	while [ -n "$*" ];
+	do
+		pkg=$1
+		ver=$2
+		file=$3
+		shift 3
+		cver=`pkginfo -l $pkg 2>&1 | awk '{if($1 == "VERSION:") { print $2;}}'`
+		if [ "$ver" != "$cver" ]; then
+			UPDATES_AVAILABLE=1
+			if [ -n "$cver" ]; then
+				echo "removing $pkg"
+				yes | pkgrm $pkg
+			fi
+			if [ -n "$ver" ]; then
+				echo "adding $pkg from $file"
+				yes | pkgadd -d $BASE/$file all
+			fi
+		fi
+	done
+}
+
+echo "Downloading package list."
+handle_packages `$CURL -s $BASE/ea.pkgs`
+
+if [ ! -r /opt/napp/etc/django-stuff/napp_stub.sqlite ]; then
+	echo "initializing web interface"
+	cp -p /opt/napp/etc/django-stuff/napp_stub.sqlite.factory \
+		/opt/napp/etc/django-stuff/napp_stub.sqlite
+fi
+if [ "$UPDATES_AVAILABLE" = "0" ]; then
+	echo "No updates."
+else
+	/usr/sbin/svcadm restart jezebel
+	/usr/sbin/svcadm restart unbound
+	/usr/sbin/svcadm restart noitd
+	/usr/sbin/svcadm restart napp
+fi
