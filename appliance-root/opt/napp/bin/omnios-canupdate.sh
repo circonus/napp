a6c29e5e37b3ff35a6dc6ca477f376d254a2c10c
Eric Sproul <esproul@omniti.com>
2012-09-19 19:30:26 +0000

    Restore proper output format. We only care about napp-incorporation because that is guaranteed to be updated whenever there is a napp or reconnoiter update.

--- appliance-root/opt/napp/bin/omnios-canupdate.sh
+++ appliance-root/opt/napp/bin/omnios-canupdate.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-UPDATES_AVAILABLE=0
+NEW_VER=""
 
 if [ -r /opt/napp/etc/napp.override ]; then
 	. /opt/napp/etc/napp.override
 fi
 
-/usr/bin/pkg update -nv napp-incorporation && UPDATES_AVAILABLE=1
+NEW_VER=$(/usr/bin/pkg update -nv napp-incorporation 2>/dev/null | ggrep -A1 napp-incorporation | tail -1 | awk '{ print $3 }')
 
-if [ "$UPDATES_AVAILABLE" = "0" ]; then
+if [ -z "$NEW_VER" ]; then
 	echo "No updates."
 	exit 1
 fi
 
+echo "napp-incorporation $NEW_VER circonus"
 exit 0
