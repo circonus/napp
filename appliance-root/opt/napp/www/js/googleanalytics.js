4757dd577b4e3bd5979c5647e18d8b9516410ef2
Philip Maddox <pmaddox@circonus.com>
2013-09-04 23:06:43 +0000

    Do Not Allow Whitespace In Google Analytics Values. Refs #1321.

--- appliance-root/opt/napp/www/js/googleanalytics.js
+++ appliance-root/opt/napp/www/js/googleanalytics.js
@@ -12,6 +12,18 @@ jQuery(function ($)
         $client_id.val($.trim($client_id.val()));
         $client_secret.val($.trim($client_secret.val()));
         $api_key.val($.trim($api_key.val()));
+        if ($client_id.val().match(/\s|'/)) {
+          alert ("Client ID cannot contain whitespace");
+          return false;
+        }
+        if ($client_secret.val().match(/\s|'/)) {
+          alert ("Client Secret cannot contain whitespace");
+          return false;
+        }
+        if ($api_key.val().match(/\s|'/)) {
+          alert ("API Key cannot contain whitespace");
+          return false;
+        }
         return true;
     }
 });
