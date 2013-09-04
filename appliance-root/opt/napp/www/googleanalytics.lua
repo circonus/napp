9eb11e76d7b2a6abbd6708d1c0b8432dc3ba179a
Philip Maddox <pmaddox@circonus.com>
2013-09-04 22:49:45 +0000

    Trim Whitespace From Google Analytics Values
    Added jQuery to trim all whitespace from beginning and end of client id,
    client secret, and api key values on submit. Refs #1321.

--- appliance-root/opt/napp/www/googleanalytics.lua
+++ appliance-root/opt/napp/www/googleanalytics.lua
@@ -82,12 +82,12 @@ http:write([=[<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"
                     Please enter the Client ID, Client Secret, and API Key associated with your Google API account below. This will allow your agent to communicate via your Google API account for Google Analytics checks.
           </p>
           <br style="clear:both"/>
-            <form id="login_form" method="POST">
+            <form id="analytics_form" method="POST">
               <p><label for="id_clientid">Client ID:</label> <input id="id_clientid" type="text" name="client_id" maxlength="500" value="]=])
 http:write(client_id)
 http:write([=["/>
               </p>
-              <p><label for="id_clientsec">Client Secret:</label> <input id="id_clientsec" type="text" name="client_secret" maxlength="500" value="]=])
+              <p><label for="id_clientsecret">Client Secret:</label> <input id="id_clientsecret" type="text" name="client_secret" maxlength="500" value="]=])
 http:write(client_secret)
 http:write([=["/>
               </p>
--- appliance-root/opt/napp/www/js/googleanalytics.js
+++ appliance-root/opt/napp/www/js/googleanalytics.js
@@ -1,3 +1,17 @@
 jQuery(function ($)
 {
+    var $form          = $("#analytics_form"),
+        $client_id     = $("#id_clientid"),
+        $client_secret = $("#id_clientsecret"),
+        $api_key       = $("#id_apikey");
+
+    $form.bind("submit", submit);
+
+    function submit(e)
+    {
+        $client_id.val($.trim($client_id.val()));
+        $client_secret.val($.trim($client_secret.val()));
+        $api_key.val($.trim($api_key.val()));
+        return true;
+    }
 });
